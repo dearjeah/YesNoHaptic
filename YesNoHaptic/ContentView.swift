46f87079c85ac8d749ff893b44856431a49e125d
del <j.delvina@gmail.com>
2026-03-06 08:31:28 +0700

    Initial Commit

---
+++ YesNoHaptic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  YesNoHaptic
+//
+//  Created by Delvina J on 06/03/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ YesNoHaptic/YesNoHapticApp.swift
@@ -0,0 +1,17 @@
+//
+//  YesNoHapticApp.swift
+//  YesNoHaptic
+//
+//  Created by Delvina J on 06/03/26.
+//
+
+import SwiftUI
+
+@main
+struct YesNoHapticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
